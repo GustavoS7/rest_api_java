50b34cf7e3e0dd57acc8de61fc5f11514d682930
unknown <GustavoS7>
2026-02-19 21:27:23 -0300

    build: conectando ao banco de dados

--- src/main/java/com/teste/primeiro_exemplo/model/Produto.java
+++ src/main/java/com/teste/primeiro_exemplo/model/Produto.java
@@ -1,6 +1,15 @@
 package com.teste.primeiro_exemplo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Produto {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Integer id;
 
   private String nome;
--- src/main/java/com/teste/primeiro_exemplo/repository/ProdutoRepository.java
+++ src/main/java/com/teste/primeiro_exemplo/repository/ProdutoRepository.java
@@ -1,71 +1,10 @@
 package com.teste.primeiro_exemplo.repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.teste.primeiro_exemplo.model.Produto;
-import com.teste.primeiro_exemplo.model.exception.ResourceNotFoundException;
 
 @Repository
-public class ProdutoRepository {
-  private List<Produto> produtos = new ArrayList<Produto>();
-  private Integer ultimoId = 0;
-
-  /**
-   * Método para retornar uma lista de produtos
-   * @return lista de produtos.
-   */
-  public List<Produto> obterTodos() {
-    return produtos;
-  }
-
-  /**
-   * Método para retornar produto pelo id
-   * @param id id do produto a ser localizado
-   * @return retorna um produto caso seja encontrado
-   */
-  public Optional<Produto> obterPorId(Integer id) {
-    return produtos
-      .stream()
-      .filter(produto -> produto.getId() == id)
-      .findFirst();
-  }
-
-  /**
-   * Método para adicionar um produto
-   * @param produto produto a ser adicionado
-   * @return retorna o produto adicionado na lista
-   */
-  public Produto adicionar(Produto produto) {
-    ultimoId++;
-    produto.setId(ultimoId);
-    produtos.add(produto);
-    return produto;
-  }
-
-  /**
-   * Método para deletar produto por id
-   * @param id id do produto a ser deletado
-   */
-  public void deletar(Integer id) {
-    produtos.removeIf(produto -> produto.getId() == id);
-  }
-
-  /**
-   * Método para atualizar produto
-   * @param produto produto a ser atualizado
-   * @return retorna o produto atualizado
-   */
-  public Produto atualizar(Produto produto) {
-    Optional<Produto> produtoEncontrato = obterPorId(produto.getId());
-    if (produtoEncontrato.isEmpty()) {
-      throw new ResourceNotFoundException("Produto não encontrado");
-    }
-    deletar(produto.getId());
-    produtos.add(produto);
-    return produto;
-  }
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 }
